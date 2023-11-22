1d349e3c955de755953ade494912a2e23218aa48
Gabriel Sá Brasil <gabrielsabrasil96@gmail.com>
2023-11-22 09:14:54 -0400

    Aula 2 Concluída

---
+++ Aula/aula2String.js
@@ -0,0 +1,17 @@
+//String
+
+const texto1 = "Olá, mundo!"
+const texto2 = "123"
+console.log(texto1)
+console.log(texto2)
+
+//Concatenação (+ ou ,)
+
+const nome = "Gabriel"
+const frase = "Meu nome é:"
+console.log(frase, nome)
+
+const idade = "27"
+const frase2 = "E tenho: "
+console.log(frase2, idade,  "anos de idade")
+
