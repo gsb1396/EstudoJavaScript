e653d7901367a67e2e72cf5077de2cab37aeaa1d
Gabriel Sá Brasil <gabrielsabrasil96@gmail.com>
2023-11-23 08:59:21 -0400

    Codificação de Caracteres Aula 2

--- Aula/aula2String.js
+++ Aula/aula2String.js
@@ -15,4 +15,13 @@ const idade = "27"
 const frase2 = "E tenho: "
 console.log(frase2, idade,  "anos de idade")
 
+// Codificação de Caracteres
 
+// const cifrao = '\u0024'
+// const bMaiuculo = '\u0042'
+// const tique = '\u2705'
+// const hiragana = '\u3041'
+// console.log(cifrao)
+// console.log(bMaiuculo)
+// console.log(tique)
+// console.log(hiragana)
