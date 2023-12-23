16faac68bf8d5bf7b664a36e94fc14221f1986da
Gabriel Sá Brasil <gabrielsabrasil96@gmail.com>
2023-12-22 22:03:13 -0400

    Aula 5 True and False

---
+++ Aula/aula5TruthyeFalse.js
@@ -0,0 +1,15 @@
+// Truhy and falsy
+// 0 = true / 1 = false
+// '==' = Compara Valores / '===' = Compara valores e tipos
+
+// console.log(true == 1)
+// console.log(true == "")
+
+// Null = Representa vazio, nada
+
+let minhaVar
+let varNull = null
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
+
