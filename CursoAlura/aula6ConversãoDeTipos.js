34be15062475ea1db366e06d1a5c8999a0c8c77b
Gabriel Sá Brasil <gabrielsabrasil96@gmail.com>
2023-12-22 23:13:36 -0400

    Aula 6 Conversão de TIpos E Atualização de Repositório

--- CursoAlura/aula6ConversãoDeTipos.js
+++ CursoAlura/aula6ConversãoDeTipos.js
@@ -11,23 +11,10 @@
 
 // Conversão Explícita
 
-// const numero = 456
-// const numeroString = "456"
+const numero = 456
+const numeroString = "456"
 
-// console.log(numero + Number(numeroString))
+console.log(numero + Number(numeroString))
 
 // Number() = Para conversão de STRING em NÙMEROS 
 // String() = Para conversão de NÙMEROS em STRING
-
-var respostaDeTudo = 42
-let idade = 29
-const pi = 3.14
-
-{
-    var respostaDeTudo = 3.14
-    let idade = 42
-    const pi = 29
-    console.log(respostaDeTudo, idade, pi)
-
-}
-console.log(respostaDeTudo, idade, pi)
