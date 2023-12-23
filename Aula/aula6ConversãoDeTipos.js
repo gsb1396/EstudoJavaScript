9a485ba10995b93eb224830f11d7f90fdc3c171b
Gabriel Sá Brasil <gabrielsabrasil96@gmail.com>
2023-12-22 22:41:37 -0400

    Aula 6 Conversão De Tipos

---
+++ Aula/aula6ConversãoDeTipos.js
@@ -0,0 +1,21 @@
+// Conversão Ímplicita
+
+// const numero = 456
+// const numeroString = "456"
+
+// console.log(numero + Number(numeroString))
+// console.log(typeof numero)
+// console.log(typeof numeroString)
+
+
+
+// Conversão Explícita
+
+const numero = 456
+const numeroString = "456"
+
+console.log(numero + Number(numeroString))
+
+// Number() = Para conversão de STRING em NÙMEROS 
+// String() = Para conversão de NÙMEROS em STRING
+
