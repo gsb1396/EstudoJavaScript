990b02de52dc2058047fe1fec00b8c4b0be27e62
Gabriel Sá Brasil <gabrielsabrasil96@gmail.com>
2023-12-23 01:13:02 -0400

    Aula 1 Do que o JavaScript é capaz

---
+++ CursoGuanabara/MóduloA/aula1OQueOJavaScriptFaz.js
@@ -0,0 +1,8 @@
+// Foi aprendido para o que serve a linguagem e a fazer algumas modificações no site do GOOGLE por meio do JavaScript
+// F12 > ... > shortcut > preferences > debugger >  DISABLE JAVASCRIPT = Desabilita o JavaScript na página em que estiver
+// console > alert('Olá, mundo!') = Retornará uma mensagem do Google
+// console > (document.body.style.background = 'black') = Retornará uma cor de sua preferência em página do Google
+// console > (document.querySelector('img#hplogo').style.filter = 'grayscale(100%)') = Retorna a logo do Google alterando sua cor
+// console > (document.querySelector('img#hplogo').style.visibility = 'hidden') = Retorna o sumiço da logo do Google
+// CTRL + L = Limpa o console do Google
+// console > (document.querySelector('span.bstn-hl-title').innerText = 'Mensagem' =  Retorna na alteração de um texto de sua preferência
