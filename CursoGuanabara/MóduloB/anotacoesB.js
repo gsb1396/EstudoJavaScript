a2f1d9ecf0c4543952f27276bbf7e6d203ce1fcf
Gabriel Sá Brasil <gabrielsabrasil96@gmail.com>
2023-12-29 01:01:40 -0400

    Início do módulo B, adição de comentários no Script e anotações.

---
+++ CursoGuanabara/MóduloB/anotacoesB.js
@@ -0,0 +1,30 @@
+// Em JavaScript o símbolo '=' significa RECEBE
+
+//  Pode declarar uma string com "", '' ou `` cada uma com sua determinada função
+
+/* Identificadores (VARIÁVEIS)
+ - Podem começar com letras, $ ou _
+ - Não podem começar com números
+ - É possível usar letras e números
+ - É possível usar acentos e símbolos
+ - Não podem conter espaços
+ - Não podem ser palavras reservadas*/
+
+/* Identificadores (VARIÁVEIS DICAS)
+ - Maiúscula e Minúsaculas fazem diferença
+ - Tente escolher nomes coerentes para as variáveis
+ - Evite se tornar um 'programador alfabeto' ou 'programador contador'*/
+
+/* Tipos Primitivos
+ - Number
+        Infinity
+        Nan
+ - String
+ - Boolean
+ - Null
+ - Undefined
+ - Object
+        Array
+ - Function     
+ 
+ (E para identificar no código de qual tipo é usamos o: TYPEOF antes da declaração)*/
