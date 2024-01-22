0ce76f5804f3083ff12693b49886a0225acc9944
Gabriel Sá Brasil <gabrielsabrasil96@gmail.com>
2024-01-22 11:41:45 -0400

    Conclusão do Exercício 17.

--- CursoGuanabara/MóduloE/Exercícios_JS/Exercício 017/script.js
+++ CursoGuanabara/MóduloE/Exercícios_JS/Exercício 017/script.js
@@ -6,14 +6,23 @@ function contar(){
     let inicioNum = Number(inicio.value)
     let fimNum = Number(fim.value)
     let passoNum = Number(passo.value)
-    if(inicioNum == 0 || fimNum == 0 || passoNum == 0){
-        window.alert('[ERRO] FALTAM DADOS')
-    } else {
-        for(let contador = inicioNum ; contador <= fimNum ; contador += passoNum){
-            resultado.innerHTML += `${contador}`
-        }
-    
+    if(inicioNum == 0 || fimNum == 0){
+        window.alert('[ERRO] Faltam Dados.')
     }
-    
+    if(passoNum == 0){
+        window.alert('[ERRO] Faltam Dados. Considerando PASSO 1')
+        passoNum = 1
+    }
+    if(inicioNum < fimNum){
+        for(let contador = inicioNum ; contador < fimNum ; contador += passoNum){
+            resultado.innerHTML += ` ${contador} \u{1F449}`    
+        }
+    } else{
+        for(let contador = inicioNum ; contador > fimNum ; contador -= passoNum){
+            resultado.innerHTML += ` ${contador} \u{1F449}`
+        }
+    }
+    resultado.innerHTML += ` \u{1F3C1} `    
+
     
 }
