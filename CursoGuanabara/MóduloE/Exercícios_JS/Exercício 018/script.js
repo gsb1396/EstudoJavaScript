ca55019a588bfbbf86b547db92287a0aa9698640
Gabriel Sá Brasil <gabrielsabrasil96@gmail.com>
2024-01-23 11:15:11 -0400

    Exercício 18 Parte lógica finalizada falta HTML e CSS

--- CursoGuanabara/MóduloE/Exercícios_JS/Exercício 018/script.js
+++ CursoGuanabara/MóduloE/Exercícios_JS/Exercício 018/script.js
@@ -1,6 +1,9 @@
-let horaSistema = new Date()
-let hora = horaSistema.getHours()
-
-switch(hora){
-    
+function gerarTabuada(){
+    let tabuada = window.document.querySelector('input#tabuada')
+    let numTabuada = Number(tabuada.value)
+    let resultado = window.document.querySelector('div#resultado')
+    resultado.innerHTML = ''
+    for(let c = 1 ; c <= 10 ; c++){
+        resultado.innerHTML += `<p> ${numTabuada} x ${c} = ${c*numTabuada} <p>>`
+    }
 }
