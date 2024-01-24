e3e3b38718d87b322176051a948824af99111108
Gabriel Sá Brasil <gabrielsabrasil96@gmail.com>
2024-01-24 11:59:51 -0400

    Fim do Módulo E. Fim do Exercício 18

--- CursoGuanabara/MóduloE/Exercícios_JS/Exercício 018/script.js
+++ CursoGuanabara/MóduloE/Exercícios_JS/Exercício 018/script.js
@@ -1,9 +1,11 @@
 function gerarTabuada(){
     let tabuada = window.document.querySelector('input#tabuada')
     let numTabuada = Number(tabuada.value)
-    let resultado = window.document.querySelector('div#resultado')
-    resultado.innerHTML = ''
+    let option = document.createElement('option')
+    let select = window.document.querySelector('select#selectTabuada')
+    select.innerHTML = ''
     for(let c = 1 ; c <= 10 ; c++){
-        resultado.innerHTML += `<p> ${numTabuada} x ${c} = ${c*numTabuada} <p>>`
+    option.text += ` ${numTabuada} x ${c} = ${c*numTabuada} `
+    select.appendChild(option)
     }
 }
