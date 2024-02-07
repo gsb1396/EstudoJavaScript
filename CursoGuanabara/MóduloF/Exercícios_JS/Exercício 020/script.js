c40b7c9929a8ba4ef6274d413f6c8fcd63c975e3
Gabriel Sá Brasil <gabrielsabrasil96@gmail.com>
2024-02-07 12:49:55 -0400

    Última atividade em andamento, o número já aparece em lista e já está dentro de um ARRAY

--- CursoGuanabara/MóduloF/Exercícios_JS/Exercício 020/script.js
+++ CursoGuanabara/MóduloF/Exercícios_JS/Exercício 020/script.js
@@ -1,7 +1,17 @@
+let num = window.document.querySelector('input#num')
+let numLista = window.document.querySelector('select#numLista')
+let numVetor = []
+
 function adicionar(){
-    let num = window.document.querySelector('input#num')
     numValor = Number(num.value)
     if(numValor < 1 || numValor > 100){
         window.alert('Favor digitar um número entre 1 e 100')
-    } 
+        return false
+    }
+    numVetor.push(numValor.value)
+    let numItem = window.document.createElement('option')
+    numItem.text = `${numValor}`
+    numLista.appendChild(numItem)
+
+    
 }
